50dce908f069b93553a73c48917e59fb874d6c60
Husooo <thuanphucle10@gmail.com>
2025-11-28 09:30:42 +0700

    Add Tutorial.h with constants and ShowTutorial function

---
+++ Tutorial.h
@@ -0,0 +1,18 @@
+// Tutorial.h
+
+#ifndef TUTORIAL_H
+#define TUTORIAL_H
+
+#include <string>
+
+static const std::string TUTORIAL_GUIDE_PATH = "C:/SnakeProject/Project/images/TutorialGuide.png";
+
+// Khai báo các hằng số tọa độ cho nút BACK (dựa trên ảnh 1024x1024)
+const float ORIGINAL_BUTTON_X = 485.0f; 
+const float ORIGINAL_BUTTON_Y = 682.0f; 
+const float ORIGINAL_BUTTON_WIDTH = 82.0f; 
+const float ORIGINAL_BUTTON_HEIGHT = 33.0f; 
+
+void ShowTutorial();
+
+#endif // TUTORIAL_H
